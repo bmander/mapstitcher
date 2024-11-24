6e65da872553a2d341dfd0a6846efbaa6dbc4e74
Brandon Martin-Anderson <badhill@gmail.com>
2024-11-24 12:05:52 -0800

    pushing the first build forward

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MapStitcher</h1>
+        <p>
+          A collaborative platform for georeferencing historical maps
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
